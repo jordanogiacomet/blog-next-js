c97f43a88bf1c46d591e424dbd0d4edf239dd454
Jordano Giacomet <jgtomazoni@ucs.br>
2025-04-26 16:19:30 -0300

    WIP - Aula 134 completa;

---
+++ src/components/PostDate/index.tsx
@@ -0,0 +1,17 @@
+import { formatDatetime, formatRelativeDate } from '@/utils/format-datetime';
+
+type PostDateProps = {
+  dateTime: string;
+};
+
+export function PostDate({ dateTime }: PostDateProps) {
+  return (
+    <time
+      className='text-slate-600 text-sm/tight'
+      dateTime={dateTime}
+      title={formatRelativeDate(dateTime)}
+    >
+      {formatDatetime(dateTime)}
+    </time>
+  );
+}
