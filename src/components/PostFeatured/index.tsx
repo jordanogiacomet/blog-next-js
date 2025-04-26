b692efa8ca29202a9d03988a149098772881e323
Jordano Giacomet <jgtomazoni@ucs.br>
2025-04-26 12:25:57 -0300

    WIP - Aula 126 completa;

--- src/components/PostFeatured/index.tsx
+++ src/components/PostFeatured/index.tsx
@@ -1,29 +1,31 @@
 import { PostCoverImage } from '../PostCoverImage';
 import { PostSummary } from '../PostSummary';
+import { findAllPublicPosts } from '@/lib/post/queries';
 
-export function PostFeatured() {
-  const slug = 'teste';
-  const postLink = `/post/${slug}`;
+export async function PostFeatured() {
+  const posts = await findAllPublicPosts();
+
+  const post = posts[0];
+
+  const postLink = `/post/${post.slug}`;
   return (
     <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
       <PostCoverImage
         linkProps={{ href: postLink }}
         imageProps={{
-          src: '/images/bryen_5.png',
+          src: post.coverImageUrl,
           width: 1200,
           height: 720,
-          alt: 'Alt da imagem',
+          alt: post.title,
           priority: true,
         }}
       />
       <PostSummary
         postHeading='h1'
         postLink={postLink}
-        createdAt={'2025-04-08T00:24:38.616Z'}
-        title={'Rotina matinal de pessoas altamente eficazes'}
-        excerpt={
-          'O Next.js também é uma boa escolha para quem quer se preocupar com performance e SEO.'
-        }
+        createdAt={post.createdAt}
+        title={post.title}
+        excerpt={post.excerpt}
       />
     </section>
   );
