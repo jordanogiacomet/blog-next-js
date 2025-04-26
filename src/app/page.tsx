f34e45d6d614d46c258a17ea57ba108bd7ad2d1a
Jordano Giacomet <jgtomazoni@ucs.br>
2025-04-25 21:01:51 -0300

    WIP - Parei no meio da aula de PostList;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import { Container } from '@/components/Container';
 import { Header } from '@/components/Header';
+import { PostCoverImage } from '@/components/PostCoverImage';
 import { PostHeading } from '@/components/PostHeading';
-import Image from 'next/image';
-import Link from 'next/link';
+import { PostsList } from '@/components/PostsList';
+import { SpinLoader } from '@/components/SpinLoader';
+import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
@@ -10,16 +12,16 @@ export default async function HomePage() {
       <Header />
 
       <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
-        <Link className='w-full h-full overflow-hidden rounded-xl' href='#'>
-          <Image
-            className='w-full h-full object-cover object-center group-hover:scale-105 transition'
-            src='/images/bryen_0.png'
-            width={1200}
-            height={720}
-            alt='Título do post'
-            priority
-          />
-        </Link>
+        <PostCoverImage
+          linkProps={{ href: '/post' }}
+          imageProps={{
+            src: '/images/bryen_5.png',
+            width: 1200,
+            height: 720,
+            alt: 'Alt da imagem',
+            priority: true,
+          }}
+        />
         <div className='flex flex-col gap-4 sm:justify-center'>
           <time className='text-slate-600 text-sm/tight' dateTime='2025-04-20'>
             20/04/2025 10:00
@@ -37,6 +39,9 @@ export default async function HomePage() {
           </p>
         </div>
       </section>
+      <Suspense fallback={<SpinLoader />}>
+        <PostsList />
+      </Suspense>
 
       <footer>
         <p className='text-6xl font-bold text-center py-8'>FOOTER</p>
--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -1,15 +1,44 @@
 import { postRepository } from '@/repositories/post';
+import { PostCoverImage } from '../PostCoverImage';
+import { PostHeading } from '../PostHeading';
 
 export async function PostsList() {
   const posts = await postRepository.findAll();
 
   return (
-    <div>
+    <div className='flex flex-col gap-8'>
       {posts.map(post => {
         return (
-          <p className='text-center' key={post.id}>
-            {post.title}
-          </p>
+          <div className='flex flex-col gap-4' key={post.id}>
+            <PostCoverImage
+              linkProps={{ href: `/post/${post.slug}` }}
+              imageProps={{
+                src: post.coverImageUrl,
+                width: 1200,
+                height: 720,
+                alt: post.title,
+              }}
+            />
+            <div className='flex flex-col gap-4 sm:justify-center'>
+              <time
+                className='text-slate-600 text-sm/tight'
+                dateTime='2025-04-20'
+              >
+                20/04/2025 10:00
+              </time>
+              <PostHeading as='h2' url='#'>
+                Lorem, ipsum dolor sit amet consectetur
+              </PostHeading>
+              <p>
+                {' '}
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum
+                architecto minus blanditiis harum excepturi delectus libero
+                velit odio recusandae saepe impedit aut natus necessitatibus
+                quos, reprehenderit consectetur deleniti temporibus quibusdam.
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </div>
+          </div>
         );
       })}
     </div>
