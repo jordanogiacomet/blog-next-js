8a18c7a99f713a15117f948468bd631f45269b1d
Jordano Giacomet <jgtomazoni@ucs.br>
2025-05-03 20:40:14 -0300

    WIP - Aula 179 completa;

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@/components/Button';
+import { InputCheckbox } from '@/components/InputCheckbox';
 import { InputText } from '@/components/InputText';
 import { Fragment } from 'react';
 
@@ -6,23 +8,33 @@ export const dynamic = 'force-dynamic';
 export default async function AdminPostNewPage() {
   return (
     <Fragment>
-      <div className='flex flex-col gap-6'>
-        <InputText labelText='Nome' placeholder='Digite seu nome' />
-        <InputText labelText='Sobrenome' placeholder='Digite seu sobrenome' />
-        <InputText
-          disabled
-          labelText='Sobrenome'
-          placeholder='Digite seu sobrenome'
-          defaultValue={'Com Texto'}
-        />
-        <InputText
-          disabled
-          labelText='Sobrenome'
-          placeholder='Digite seu sobrenome'
-          defaultValue={'Read Only'}
-          readOnly
-        />
-      </div>
+      <form action='' className='mb-16'>
+        <div className='flex flex-col gap-6'>
+          <InputText labelText='Nome' placeholder='Digite seu nome' />
+          <InputText labelText='Sobrenome' placeholder='Digite seu sobrenome' />
+          <InputText
+            disabled
+            labelText='Sobrenome'
+            placeholder='Digite seu sobrenome'
+            defaultValue={'Com Texto'}
+          />
+
+          <InputCheckbox type='checkbox' />
+
+          <InputText
+            disabled
+            labelText='Sobrenome'
+            placeholder='Digite seu sobrenome'
+            defaultValue={'Read Only'}
+            readOnly
+          />
+          <div>
+            <Button type='submit' size='lg' className='w-full mt-6'>
+              Enviar
+            </Button>
+          </div>
+        </div>
+      </form>
     </Fragment>
   );
 }
