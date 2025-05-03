5526b02892762bac4ad752cb6af3433c63c627ea
Jordano Giacomet <jgtomazoni@ucs.br>
2025-05-03 11:29:54 -0300

    WIP - Aula 168 completa;

--- src/components/PostsListAdmin/index.tsx
+++ src/components/PostsListAdmin/index.tsx
@@ -1,7 +1,7 @@
 import { findAllPostAdmin } from '@/lib/post/queries/admin';
 import clsx from 'clsx';
-import { Trash2Icon } from 'lucide-react';
 import Link from 'next/link';
+import { DeletePostButton } from '../Admin/DeletePostButton';
 
 export default async function PostsListAdmin() {
   const posts = await findAllPostAdmin();
@@ -24,21 +24,53 @@ export default async function PostsListAdmin() {
                 (Não publicado)
               </span>
             )}
-            <button
-              className={clsx(
-                'text-red-500 cursor-pointer transition',
-                '[&_svg]:w-4 [&_svg]:h-4',
-                'sm:[&_svg]:w-5 sm:[&_svg]:h-5',
-                'hover:scale-120 hover:text-red-700',
-              )}
-              aria-label={`Apagar post: ${post.title}`}
-              title={`Apagar post: ${post.title}`}
-            >
-              <Trash2Icon />
-            </button>
+            <DeletePostButton title={post.title} id={post.id} />
           </div>
         );
       })}
+      <div
+        className={clsx(
+          'fixed z-50 bg-black/50 backdrop-blur-xs inset-0',
+          'flex items-center justify-center',
+        )}
+      >
+        <div
+          className={clsx(
+            'bg-slate-100 p-6 rounded-lg max-w-2xl mx-6',
+            'flex flex-col gap-6',
+            'shadow-lg shadow-black/30 text-center',
+          )}
+        >
+          <h3 className='text-xl font-extrabold'>Título do diálogo</h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima quo
+            blanditiis deserunt tempora dolorem laboriosam explicabo, numquam,
+            rem illum eveniet facilis, molestiae obcaecati aperiam. Maxime et
+            doloremque est sit minima?
+          </p>
+          <div className='flex items-center justify-around'>
+            <button
+              className={clsx(
+                'bg-slate-300 text-slate-950 hover:bg-slate-400 transition',
+                'flex items-center justify-center',
+                'py-2 px-4 rounded-lg cursor-pointer',
+              )}
+              autoFocus
+            >
+              Cancelar
+            </button>
+            <button
+              className={clsx(
+                'bg-blue-500 text-blue-50 hover:bg-blue-600 transition',
+                'flex items-center justify-center',
+                'py-2 px-8 rounded-lg cursor-pointer',
+              )}
+            >
+              Ok
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
